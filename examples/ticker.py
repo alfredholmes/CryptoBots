a5a5ad9b0e08546ce7c3477463b7a0a43942d45e
Alfred <holmesey55@gmail.com>
2023-02-25 15:38:05 +0000

    ftx removal

--- examples/ticker.py
+++ examples/ticker.py
@@ -23,13 +23,12 @@ async def main(args):
 
     #connect to FTX spot
     print('connecting...')
-    async with FTX() as ftx, Binance() as binance:
+    async with Binance() as binance:
         print('subscribing to order books')
-        await ftx.subscribe_to_order_books(('BTC', 'USD'))
         await binance.subscribe_to_order_books(('BTC', 'USDT'))
 
         for i in range(10):
-            print(ftx.order_books[('BTC', 'USD')].mid_price(), binance.order_books[('BTC', 'USDT')].mid_price())
+            print(binance.order_books[('BTC', 'USDT')].mid_price())
             await asyncio.sleep(1)
 
 if __name__=='__main__':
